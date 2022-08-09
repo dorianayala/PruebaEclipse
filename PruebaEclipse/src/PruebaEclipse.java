ea37f43df635545ebfd446b1ee0defa63cd63ba7
dorianayala <dorian_hotmailu@hotmaile.com>
2022-08-09 16:32:25 -0500

    Primer Commit

---
+++ PruebaEclipse/src/PruebaEclipse.java
@@ -0,0 +1,10 @@
+
+
+public class PruebaEclipse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bienvenidos a la prueba de GIT");
+	}
+
+}
